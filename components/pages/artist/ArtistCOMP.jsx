3edd78cd529bfa95562fea1411c97e8919851b4a
isrezei <rezaeiism@yahoo.com>
2022-12-22 00:34:16 +0330

    *add artist albums to artist page

--- components/pages/artist/ArtistCOMP.jsx
+++ components/pages/artist/ArtistCOMP.jsx
@@ -9,6 +9,7 @@ import {RiPlayFill} from 'react-icons/ri'
 import Tilt from "react-parallax-tilt";
 import {PUT_SPOTIFY_PLAY_MUSIC} from "../../../lib/PuterFuncs/PUT_SPOTIFY_PLAY_MUSIC";
 import prettyMilliseconds from "pretty-ms";
+import {ScrollContainer} from "react-indiana-drag-scroll";
 
 export const ArtistCOMP = () =>
 {
@@ -31,9 +32,10 @@ export const ArtistCOMP = () =>
 
                 <Img src={ARTIST?.artist_info.images[0].url} w={'full'} h={'100vh'} objectFit={'cover'} opacity={'30%'} position={"absolute"}/>
 
-                <Flex w={"full"} direction={'column'} position={"absolute"} >
+                <Flex w={"full"} h={'100vh'} p={5} direction={'column'} position={"absolute"} overflowY={"scroll"} overflowX={'hidden'} >
 
-                    <VStack justify={"center"} align={'start'} w={'full'} h={'30vh'} px={5} >
+
+                    <VStack justify={"center"} align={'start'} w={'full'} h={'40vh'} px={5} >
                         <Text fontWeight={'bold'} fontSize={'5vw'}>{ARTIST?.artist_info.name}</Text>
                         <Text fontSize={'1.2vw'}>{ARTIST?.artist_info.followers.total} Followers </Text>
 
@@ -46,13 +48,14 @@ export const ArtistCOMP = () =>
                         </HStack>
                     </VStack>
 
-                    <VStack w={"full"} h={'70vh'} >
 
-                        <VStack w={"full"} align={"start"}>
+
+                    <VStack justify={'start'} align={'start'} w={"full"}>
+
+                        <VStack w={"full"}  align={"start"}>
                             <Text>Popular</Text>
-
                             {
-                                ARTIST?.top_track.reverse().splice(0 , 5).map((TRACKS , INDEX) => (
+                                ARTIST?.top_track.map((TRACKS , INDEX) => (
                                     <Box w={'full'} key={TRACKS.id}>
                                         <Tilt tiltEnable={false} glareEnable={true} glareBorderRadius={'.8vw'} glareMaxOpacity={0.3} glareColor="#6d6d6d" glarePosition="all">
                                             <Flex key={Math.random()}
@@ -77,8 +80,8 @@ export const ArtistCOMP = () =>
                                                     </Box>
                                                 </Flex>
 
-                                                <Text flex={1}  textAlign={'center'}>{TRACKS?.track?.album?.name}</Text>
-                                                <Text flex={1}  textAlign={'center'}>{TRACKS?.added_at}</Text>
+                                                <Text flex={1}  textAlign={'center'}>{TRACKS?.album?.name}</Text>
+
                                                 <Text flex={1}  textAlign={'center'}>  {prettyMilliseconds(TRACKS?.duration_ms , {secondsDecimalDigits : 0 , colonNotation : true})}</Text>
                                             </Flex>
                                         </Tilt>
@@ -88,6 +91,24 @@ export const ArtistCOMP = () =>
 
                         </VStack>
 
+
+                        <Text fontWeight={'bold'} fontSize={'2vw'}>More albums from {ARTIST?.artist_info.name}</Text>
+
+
+                            <ScrollContainer style={{display : 'flex' , width : '100%'}}>
+                                {
+                                    ARTIST?.artist_albums.items.map(ALBUMS => (
+
+                                        <VStack  key={ALBUMS.id} flex={'none'} bg={"blackAlpha.500"} p={1} mr={3} rounded={'.8vw'} _hover={{ bg: "#212121"}}>
+                                            <Image src={ALBUMS?.images[0].url} boxSize={'15vw'} p={3} rounded={'1.5vw'} alt=''/>
+                                            <Text  textAlign={'left'} fontWeight={'bold'} fontSize={'md'} align={'left'} color={'whitesmoke'}>{ALBUMS?.name}</Text>
+                                            <Text  textAlign={'left'} fontSize={'.8vw'} align={'left'} color={'#9e9e9e'}>{ALBUMS?.artists[0]?.name}</Text>
+                                        </VStack>
+
+                                    ))
+
+                                }
+                            </ScrollContainer>
 
                     </VStack>
 
