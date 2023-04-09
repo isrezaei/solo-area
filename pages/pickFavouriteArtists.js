8ba607caa377136bc953c4fe5d9062b74e37cf98
isrezei <rezaeiism@yahoo.com>
2023-04-09 18:55:59 +0430

    rebuild project

--- pages/pickFavouriteArtists.js
+++ pages/pickFavouriteArtists.js
@@ -1,20 +1,6 @@
-import {FETCH_SEVERAL_ARTIST} from "../lib/FetcherFuncs/FETCH_SEVERAL_ARTIST";
 import {PickFavouriteArtists} from "../components/PickFavouriteArtists";
 
-export default function pickFavouriteArtists ({getSeveralArtist : {artists}})
+export default function pickFavouriteArtists ()
 {
-    return <PickFavouriteArtists getSeveralArtist={artists}/>
-}
-
-
-export const getStaticProps = async () =>
-{
-    const getSeveralArtist = await FETCH_SEVERAL_ARTIST()
-
-
-    return {
-        props : {
-            getSeveralArtist
-        }
-    }
+    return "none"
 }
