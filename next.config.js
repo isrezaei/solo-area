7b2b661e0d4d760ccf5d4e76ed38c42231fb9a55
isrezei <rezaeiism@yahoo.com>
2023-04-12 17:33:59 +0430

    work on pwa for all route !

--- next.config.js
+++ next.config.js
@@ -4,8 +4,13 @@ const withPWA = require('next-pwa')({
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
     register: true,
+    scope: "/",
     skipWaiting: true,
-    sw: 'service-worker.js'
+    sw: 'service-worker.js',
+    icon: {
+        // path: 'static/logo.png',
+        // purpose: 'maskable any',
+    }
 })
 
 module.exports = withPWA({
