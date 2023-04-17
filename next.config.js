5f3d44ff2a7a1940a5ccb122e335e60656cbf2a0
isrezei <rezaeiism@yahoo.com>
2023-04-17 22:51:18 +0430

    maybe this work !

--- next.config.js
+++ next.config.js
@@ -3,15 +3,14 @@
 const withPWA = require('next-pwa')({
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
-    scope: "/",
-    sw: 'service-worker.js'
+    scope: "/"
 })
 
 module.exports = withPWA({
     reactStrictMode: true,
     swcMinify: true,
     images: {
-        domains: ['i.scdn.co', 't.scdn.co', 'user-images.githubusercontent.com'],
+        domains: ['i.scdn.co', 't.scdn.co', 'user-images.githubusercontent.com']
     },
     eslint: {
         ignoreDuringBuilds: true
